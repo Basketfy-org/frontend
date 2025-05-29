1134df0a2d47cf626578e08be8ee703bab6823a2
Ayo Solomon <48213053+Ayoseun@users.noreply.github.com>
2025-05-29 04:27:56 +0100

    .

--- src/src/pages/portfolio.jsx
+++ src/src/pages/portfolio.jsx
@@ -26,7 +26,7 @@ export const PortfolioPage = ({ darkMode, investAmount, selectedBasket }) => {
                     </div>
                     <div className="flex items-center gap-4">
                         <button
-                                   onClick={() =>  navigate(`explore`)}
+                                   onClick={() =>  navigate(`/explore`)}
                             className="bg-purple-600 hover:bg-purple-700 text-white px-4 py-2 rounded-lg transition-colors"
                         >
                             Explore More
